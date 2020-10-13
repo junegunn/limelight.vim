junegunn/limelight.vim

commit 815c6122504361d52e10172601471b416d040ca8

Don't move the cursor to beginning of line on deactivating limelight (#60)
Calling functions in vim using a range with `<line1>,<line2>call ...`
makes vim move the cursor to the beginning of the line. This can cause
distraction when Limelight is used in conjunction with the insert mode
(e.g. when deactivating on `InsertLeave`).

This commit makes the `Limelight` command pass the range as parameters
to the `execute` function which removes that effect while
`Limelight`ening a range still works.

autoload/limelight.vim
@@ -254,8 +254,8 @@ function! s:cleanup()
   end
 endfunction
 
-function! limelight#execute(bang, visual, ...) range
-  let range = a:visual ? [a:firstline, a:lastline] : []
+function! limelight#execute(bang, visual, line1, line2, ...)
+  let range = a:visual ? [a:line1, a:line2] : []
   if a:bang
     if a:0 > 0 && a:1 =~ '^!' && !s:is_on()
       if len(a:1) > 1

plugin/limelight.vim
@@ -21,7 +21,7 @@
 " OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 " WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-command! -nargs=? -bar -bang -range Limelight <line1>,<line2>call limelight#execute(<bang>0, <count> > 0, <f-args>)
+command! -nargs=? -bar -bang -range Limelight call limelight#execute(<bang>0, <count> > 0, <line1>, <line2>, <f-args>)
 
 nnoremap <silent> <Plug>(Limelight) :set opfunc=limelight#operator<CR>g@
 xnoremap <silent> <Plug>(Limelight) :Limelight<CR>

commit 2adb5e2caccd682b6043e1a9bc6105da495b1896

Fix normal mode operator: <Plug>(Limelight)
Caused by #60

autoload/limelight.vim
@@ -274,7 +274,7 @@ function! limelight#execute(bang, visual, line1, line2, ...)
 endfunction
 
 function! limelight#operator(...)
-  '[,']call limelight#execute(0, 1)
+  call limelight#execute(0, 1, line("'["), line("']"))
 endfunction
 
 let &cpo = s:cpo_save